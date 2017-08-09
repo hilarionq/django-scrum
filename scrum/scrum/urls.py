2da4bb3518a7070168ce9d78de43cba5ad9158f7
hilarionq <hilarionq@gmail.com>
2017-08-09 18:25:07 -0400

    Creating models.

--- scrum/scrum/urls.py
+++ scrum/scrum/urls.py
@@ -18,5 +18,5 @@ from django.conf.urls import url, include
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-        url(r'^api/token/', obtain_auth_token, name='api_token'),
+        url(r'^api/token/', obtain_auth_token, name='api-token'),
 ]
