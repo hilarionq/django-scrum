05c93750c94bbd0f7234f0433e017b5d57668ae3
hilarionq <hilarionq@gmail.com>
2017-08-10 11:41:54 -0400

    Connecting to the router.

--- scrum/board/serializers.py
+++ scrum/board/serializers.py
@@ -13,8 +13,7 @@ class SprintSerializer(serializers.ModelSerializer):
 
 class TaskSerializer(serializers.ModelSerializer):
 
-    assigned =  serializers.SlugRelatedField(
-        slug_field=User.USERNAME_FIELD, required = False)
+    assigned =  serializers.SlugRelatedField(slug_field=User.USERNAME_FIELD, required=False, read_only=True)
     status_display = serializers.SerializerMethodField('get_status_display')
 
     class Meta:
@@ -26,9 +25,7 @@ class TaskSerializer(serializers.ModelSerializer):
         return obj.get_status_display()
 
 class UserSerializer(serializers.ModelSerializer):
-    
     full_name = serializers.CharField(source='get_full_name', read_only=True)
-
     class Meta:
         model = User
         fields = ('id', User.USERNAME_FIELD, 'full_name', 'is_active',)
